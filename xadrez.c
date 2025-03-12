f37f92ebffc4b797a5d88792af5d533e8ae742bd
Pedro-LM1 <pedro.lucasmf1@gmail.com>
2025-03-12 14:34:12 +0000

    Adicionado a logica da peça Cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,14 +2,14 @@
 
 
 int main(){
-    int bispo = 5, rainha = 8;
+    int bispo = 5, rainha = 8, cavalo, movimento_L = 1;
 
-    printf("\n## Movimento para torre: \n");
+    printf("\n## Jogada para torre: \n");
     for (int torre = 0; torre < 5; torre++){
         printf("Direita\n");
     }
 
-    printf("\n## Movimento para bispo\n");
+    printf("\n## Jogada para bispo\n");
     while (bispo > 0)
     {
         printf("Direita-Cima\n");
@@ -17,13 +17,24 @@ int main(){
     }
     
 
-    printf("\n## Movimento para rainha\n");
+    printf("\n## Jogada para rainha\n");
     do
     {
         printf("Esquerda\n");
         rainha--;
     } while (rainha > 0);
     
+
+    printf("\n## Jogada para cavalo\n");
+    while(movimento_L--)
+    {
+        for(cavalo = 0; cavalo < 2; cavalo++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
+
 
     return 0;
 }
